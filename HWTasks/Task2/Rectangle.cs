ed0095bab0ed9639f7ac8077bb9fcc13dd469e01
dvlprJacob <machomen91108@gmail.com>
2017-11-29 12:21:59 +0400

    Task02

---
+++ HWTasks/Task2/Rectangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    /// <summary>
+    /// Subtask2_1
+    /// Написать программу, которая определяет площадь прямоугольника со сторонами a и b.
+    /// Если пользователь вводит некорректные значения(отрицательные, или 0),должно выдаваться сообщение об ошибке.
+    /// Возможность ввода пользователем строки вида «абвгд», или нецелых чисел игнорировать.
+    /// </summary>
+    internal class Rectangle
+    {
+        private int a;
+        private int b;
+
+        public int Width
+        {
+            get { return a; }
+            set
+            {
+                if (value > 0)
+                {
+                    a = value;
+                    return;
+                }
+            }
+        }
+
+        public int Height
+        {
+            get { return b; }
+            set
+            {
+                if (value > 0)
+                {
+                    b = value;
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Площадь прямоугольника
+        /// </summary>
+        public int Area { get { return Width * Height; } }
+
+        public Rectangle(int width, int height)
+        {
+            if (width <= 0 || height <= 0)
+                throw new ArgumentException("Arguments values must be more than zero");
+
+            Width = width;
+            Height = height;
+        }
+
+        public Rectangle()
+        {
+        }
+    }
+}
---
+++ HWTasks/Task2/Subtask2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    /// <summary>
+    /// Subtask2_2 Лесенка
+    /// </summary>
+    internal class Subtask2
+    {
+        private int n;
+
+        public int RowCount
+        {
+            get { return n; }
+            set { if (value > 0) n = value; }
+        }
+
+        public Subtask2(int rowCount)
+        {
+            RowCount = rowCount;
+        }
+
+        public Subtask2()
+        {
+        }
+
+        public override string ToString()
+        {
+            if (RowCount == 1)
+                return "*";
+
+            String temporary = "*\n";
+            for (int i = 2; i < RowCount; i++)
+            {
+                temporary += new String('*', i) + "\n";
+            }
+            temporary += new string('*', RowCount);
+            return temporary.ToString();
+        }
+    }
+}
